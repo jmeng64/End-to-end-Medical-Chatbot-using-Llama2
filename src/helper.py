d39c41560239aeb8609f411e8e77309920a67aa9
Jerry Meng <jmeng64@gmail.com>
2024-08-24 21:58:17 -0400

    8/24/24 still working on it.

--- src/helper.py
+++ src/helper.py
@@ -17,14 +17,11 @@ def load_pdf(data):
 
 #Create text chunks
 def text_split(extracted_data):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
-    text_chunks = text_splitter.split_documents(extracted_data)
-
-    return text_chunks
+    return  RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20).split_documents(extracted_data)
 
 
 
 #download embedding model
 def download_hugging_face_embeddings():
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    return embeddings
+    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    
--- store_index.py
+++ store_index.py
@@ -1,5 +1,6 @@
 from src.helper import load_pdf, text_split, download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
+#from langchain.vectorstores import Pinecone
+from langchain_pinecone import Pinecone  
 import pinecone
 from dotenv import load_dotenv
 import os
@@ -14,15 +15,21 @@ PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
 
 extracted_data = load_pdf("data/")
 text_chunks = text_split(extracted_data)
-embeddings = download_hugging_face_embeddings()
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
 
 
 #Initializing the Pinecone
-pinecone.init(api_key=PINECONE_API_KEY,
-              environment=PINECONE_API_ENV)
+# pinecone.init(api_key=PINECONE_API_KEY,
+#             environment=PINECONE_API_ENV)
+
+pc=pinecone.Pinecone(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV,
+              region="us-east-1")
 
 
-index_name="medical-bot"
+index_name="medical-chatbot"
+index = pc.Index(index_name)
 
 #Creating Embeddings for Each of The Text Chunks & storing
 docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
